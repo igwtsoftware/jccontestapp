2de6c2e4855224ae023468b844b16c15b8a07440
igwtsoftware@gmail.com <igwtsoftware@gmail.com>
2014-11-23 20:09:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jccontestapp::Application.config.secret_key_base = '5c9e51dc04165ffb02d996b9706eb0432ae41113d19bcafdf0f3c72f838674fae3c7aeef5e431a71222a3a82b6ce640ac0c76a2bbb35b66afb877755eeb7cf09'
